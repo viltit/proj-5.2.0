6329832f9a329b4369971a0c9280483a9d2a17d9
viltit <villiger.titus@bluewin.ch>
2019-07-11 16:21:52 +0200

    Create proj-5.2.0.rb

---
+++ proj-5.2.0.rb
@@ -0,0 +1,58 @@
+class Proj < Formula
+  desc "Cartographic Projections Library"
+  homepage "https://proj4.org/"
+  url "https://download.osgeo.org/proj/proj-5.2.0.tar.gz"
+  sha256 "676165c54319d2f03da4349cbd7344eb430b225fe867a90191d848dc64788008"
+
+  bottle do
+    sha256 "63be7d4fa4555300a5b64fa243f1b22597ae0622b0eaa88397919dd102dc83e1" => :mojave
+    sha256 "baa0d9bbe0f5a22baaa29c4dfff3cadfbd070bb3072fc401217f7f9bf130d95f" => :high_sierra
+    sha256 "df7076eda0eb6e27a90ccd3db0746ca42072154e20e1a054301af66ae32d1a34" => :sierra
+  end
+
+  head do
+    url "https://github.com/OSGeo/PROJ/tree/5.2"
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "libtool" => :build
+  end
+
+  depends_on "pkg-config" => :build
+
+  conflicts_with "blast", :because => "both install a `libproj.a` library"
+
+  skip_clean :la
+
+  # The datum grid files are required to support datum shifting
+  resource "datumgrid" do
+    url "https://download.osgeo.org/proj/proj-datumgrid-1.8.zip"
+    sha256 "b9838ae7e5f27ee732fb0bfed618f85b36e8bb56d7afb287d506338e9f33861e"
+  end
+
+  def install
+    (buildpath/"nad").install resource("datumgrid")
+
+    system "./autogen.sh" if build.head?
+    system "./configure", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}"
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test").write <<~EOS
+      45d15n 71d07w Boston, United States
+      40d40n 73d58w New York, United States
+      48d51n 2d20e Paris, France
+      51d30n 7'w London, England
+    EOS
+    match = <<~EOS
+      -4887590.49\t7317961.48 Boston, United States
+      -5542524.55\t6982689.05 New York, United States
+      171224.94\t5415352.81 Paris, France
+      -8101.66\t5707500.23 London, England
+    EOS
+
+    output = shell_output("#{bin}/proj +proj=poly +ellps=clrk66 -r #{testpath}/test")
+    assert_equal match, output
+  end
+end
